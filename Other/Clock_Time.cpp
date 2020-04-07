adc7d11c8f24f5c6c84f7055789b056ff5691fac
Hasibul Haque Himel <32277706+fire-tornado@users.noreply.github.com>
2020-04-08 02:02:48 +0600

    Create Clock_Time.cpp

---
+++ Other/Clock_Time.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Fast            ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define fWrite          freopen ("out.txt","w",stdout);
+#define TC              int t;cin >> t;FOR(tc,1,t)
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define pii             pair<int,int>
+#define all(a)          a.begin(),a.end()
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+
+int main()
+{
+    clock_t start,finish;
+    double timespent;
+    start=clock();
+
+    //Here Comes your Code/Function
+
+    finish=clock();
+    timespent=(double)(finish-start)/CLOCKS_PER_SEC;
+    cout << timespent << '\n';
+    return 0;
+}
